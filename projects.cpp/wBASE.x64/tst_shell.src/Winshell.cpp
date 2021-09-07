5e9e77c966cb41268423b2ac2d4cfaef66a6b950
keith r. bergerstock <krbergerstock@e4kountdown.com>
2021-09-07 06:11:39 -0400

    fixed errors from port rearranged file locations

---
+++ projects.cpp/wBASE.x64/tst_shell.src/Winshell.cpp
@@ -0,0 +1,82 @@
+// csn be used as 32 or 64 bit start point
+// library converted to use universal window functions
+// keith r. bergerstock  2021-08-25
+
+#ifndef __WINDOWS_H
+#define STRICT
+#include <windows.h>
+#include <windowsx.h>
+#endif
+
+#include "WinSHELL.h"
+#include "cMUTEX.h"
+#include "cDIALOG.H"
+#include "ABOUT.H"
+#include "resource.h"
+
+cWINSHELL::cWINSHELL(HINSTANCE hi,int nc) : cAppWinFrame(hi,nc){
+    SetMenu(IDR_MENU1);
+    SetLargeIcon(IDI_ICON1);
+    SetSmallIcon(IDI_ICON1);
+    if(Create() ) {
+        Show(nc);
+        Run();
+    }        
+
+}
+
+cWINSHELL::~cWINSHELL(){ }
+
+LRESULT cWINSHELL::WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+
+    switch (msg){
+        HANDLE_MSG(hwnd, WM_COMMAND, OnCommand);
+    }
+    return cAppWinFrame::WndProc(hwnd, msg, wParam, lParam);
+}
+
+void cWINSHELL::OnCommand(HWND hwnd, int id, HWND hwndCtl, UINT codeNotify)
+{
+    switch (id)
+    {
+
+        case IDM_EXIT:
+            ExitWindow();
+            break;
+
+        case IDM_ABOUT_CMD:
+        {
+            pDIALOGBOX pBox = new AboutBox(hInstance);
+            pBox->Display(hwnd, "AboutBox");
+            delete pBox;
+        }
+        break;
+
+        default:
+            cAppWinFrame::OnCommand(hwnd, id, hwndCtl, codeNotify);
+            break;
+    }
+}
+
+void cWINSHELL::Paint(HDC hdc) {
+
+}
+
+int WINAPI WinMain (HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+    // the following three lines verify that only one copy of this program
+    // is capable of runing under a win32 enviroment.
+    tMUTEX prg(true,"WINSHELL");
+    if(!prg.doesExist() )
+     {
+        
+         cWINSHELL shell(hInstance,nCmdShow);
+
+     }
+    else
+     {
+        MessageBox(NULL,"Application already running","ERROR",MB_OK);
+     }
+
+    return 0;
+}
---
+++ projects.cpp/wBASE.x64/tst_shell.src/Winshell.h
@@ -0,0 +1,37 @@
+#ifndef __WINDOWS_H
+#ifndef STRICT
+#define STRICT
+#endif
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <windowsx.h>
+#endif
+
+#include "cAppBase.h"
+
+#ifndef __WINSHELL_h
+#define __WINSHELL_h
+
+
+class cWINSHELL : public cAppWinFrame
+{
+
+    private:
+		// message cracker handling routines
+		void OnCommand(HWND, int, HWND, UINT);
+
+		// paint routine
+		void Paint(HDC);
+
+		// message handler
+		LRESULT WndProc(HWND, UINT, WPARAM, LPARAM);
+
+    public:
+
+        cWINSHELL(HINSTANCE,int);
+        ~cWINSHELL();
+};
+
+#endif
